02d0bcaa026e48bb46706d5ef3c87d2b21c15852
Yusuf Zaenul Mustofa <yusufmustofa930@gmail.com>
2024-02-21 23:37:40 +0700

    Pertemuan 1

--- Praktikum-01/Modul/MTitik.java
+++ Praktikum-01/Modul/MTitik.java
@@ -1,20 +1,19 @@
+/********************************************************************************/
+/* File      : MTitik.java                                                      */
+/* Deskripsi : Kelas yang berisi main dari Titik                                */
+/* NIM/Nama  : 24060122120021/Yusuf Zaenul Mustofa                              */
+/* Tanggal   : 21 Februari 2024                                                 */
+/********************************************************************************/
+
 public class MTitik {
     public static void main(String[] args){
         Titik t1;
         Titik t2;
         Titik t3;
 
-        t1 = new Titik();
-        t1.setAbsis(1);
-        t1.setOrdinat(2);
-
-        t2 = new Titik();
-        t2.setAbsis(3);
-        t2.setOrdinat(4);
-
-        t3 = new Titik();
-        t3.setAbsis(5);
-        t3.setOrdinat(6);
+        t1 = new Titik(1,2);
+        t2 = new Titik(3,4);
+        t3 = new Titik(5,6);
         
         int counterTitik = Titik.getCounterTitik();
 
@@ -22,8 +21,5 @@ public class MTitik {
         System.out.printf("t1(%.1f, %.1f)\n", t1.getAbsis(), t1.getOrdinat());
         System.out.printf("t2(%.1f, %.1f)\n", t2.getAbsis(), t2.getOrdinat());
         System.out.printf("t3(%.1f, %.1f)\n", t3.getAbsis(), t3.getOrdinat());
-        // System.out.printf("Titik t1 dengan absis : %.2f, ordinat : %.2f\n", t1.getAbsis(), t1.getOrdinat());
-        // System.out.printf("Titik t2 dengan absis : %.2f, ordinat : %.2f\n", t2.getAbsis(), t2.getOrdinat());
-        // System.out.printf("Titik t3 dengan absis : %.2f, ordinat : %.2f\n", t3.getAbsis(), t3.getOrdinat());
     }
 }
--- Praktikum-01/Modul/Titik.java
+++ Praktikum-01/Modul/Titik.java
@@ -1,34 +1,39 @@
+
+/********************************************************************************/
+/* File      : Titik.java                                                      */
+/* Deskripsi : Kelas Titik yang merupakan sebuah titik dengan absis dan ordinat */
+/* NIM/Nama  : 24060122120021/Yusuf Zaenul Mustofa                              */
+/* Tanggal   : 21 Februari 2024                                                 */
+/********************************************************************************/
+
 public class Titik{
-    //Atribut
+    /*Atribut*/
     double absis;
     double ordinat;
-    static int counterTitik;
+    static int counterTitik; 
 
-    //Konstruktor
-    Titik(){
+    /*Konstruktor*/
+    Titik(int a, int o){
         counterTitik++;
-        absis = 0;
-        ordinat = 0;
+        absis = a;
+        ordinat = o;
     }
 
-    //Mutator
+    /*Mutator*/ 
     void setAbsis(double a){
         absis = a;
     }
-
     void setOrdinat(double o){
         ordinat = o;
     }
 
-    //Selektor
+    /*Selektor*/
     double getAbsis(){
         return absis;
     }
-
     double getOrdinat(){
         return ordinat;
     }
-
     static int getCounterTitik(){
         return counterTitik;
     }
