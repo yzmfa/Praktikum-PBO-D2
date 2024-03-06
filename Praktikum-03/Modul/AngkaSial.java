d9158f663df15db395a97ce8a667dde94d420259
Yusuf Zaenul Mustofa <yusufmustofa930@gmail.com>
2024-03-06 10:56:36 +0700

    Pertemuan 3

---
+++ Praktikum-03/Modul/AngkaSial.java
@@ -0,0 +1,27 @@
+/*
+ * File      : AngkaSial.java
+ * Deskripsi : Program penggunaan exception buatan sendiri dan pengenalan klausa 'throw' dan 'throws'
+ */
+public class AngkaSial {
+    public void cobaAngka(int angka) throws AngkaSialException{
+        if (angka == 13){
+            throw new AngkaSialException();
+        }
+        System.out.println(angka + " bukan angka sial");
+    }
+
+    public static void main(String[] args) {
+        AngkaSial as = new AngkaSial();
+        try{
+            as.cobaAngka(10);
+            as.cobaAngka(13);
+            as.cobaAngka(12);
+        }
+        catch (AngkaSialException ase){
+            //Method getMessage() telah ada pada kelas "Exception"
+            System.out.println(ase.getMessage());
+            System.out.println("hati-hati memasukkan angka!!!");
+        }
+    }
+}
+
---
+++ Praktikum-03/Modul/ExceptionOnArray.java
@@ -0,0 +1,19 @@
+/*
+ * File      : ExceptionOnArray.java
+ * Deskripsi : Program penggunaan eksepsi menggunakan class librari Java
+ */
+public class ExceptionOnArray {
+    public static void main(String[] args) {
+        Integer[] arrayInteger = new Integer[4];
+        try{
+            arrayInteger[2] = 11;
+            arrayInteger[4] = 10;
+        }
+        catch (ArrayIndexOutOfBoundsException exception){
+            exception.printStackTrace();
+        }
+        finally {
+            System.out.println("clean up code...");
+        }
+    }
+}
