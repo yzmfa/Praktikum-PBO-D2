0698444d6c1eb7f5680cc7f9e7eb0ddb6267c708
Yusuf Zaenul Mustofa <yusufmustofa930@gmail.com>
2024-05-22 23:20:33 +0700

    Pertemuan 10

---
+++ Praktikum-10/Modul/PenjelasanNo4.java
@@ -0,0 +1,5 @@
+/*
+ * Implementasi List dengan ArrayList mendukung akses elemen berdasarkan indeks dan memungkinkan duplikasi elemen, meskipun operasi di tengah daftar bisa lebih lambat. 
+ * Implemetasi Set dengan HashSet tidak mengizinkan duplikasi elemen dan sangat efisien dalam pencarian, namun tidak memiliki akses indeks dan tidak menjamin urutan elemen. 
+ * Implementasi Map dengan HashMap menyimpan pasangan kunci-nilai, mendukung pencarian cepat berdasarkan kunci, tetapi tidak mendukung duplikat kunci dan urutan elemen tidak dijamin.
+ */
